8f2d3ca438dbbecd5c4a2c7fbd1b42452f3617ee
Marcin Rataj <marcinrataj0@gmail.com>
2023-10-06 08:38:20 +0200

    add year in JS for footer section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btb-bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
   nav.classList.toggle('nav--active');
@@ -23,4 +26,13 @@ const handleNavItemsAnimation = () => {
     delayTime++;
   })
 }
+
+
+
+const handleCurrentYear = () => {
+  const year = (new Date).getFullYear();
+  footerYear.innerText = year;
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav);
